871c508fddb434553657a4f982599302eb282df5
Mortuza Hossain <mortuzahossain1997@gmail.com>
2017-10-16 04:20:03 +0600

    Done Written Database SQl for creating the data table

---
+++ database.sql
@@ -0,0 +1,34 @@
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `university` varchar(100) NOT NULL,
+  `current_level` int(1) NOT NULL,
+  `current_tearm` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+CREATE TABLE IF NOT EXISTS `data` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `universityName` varchar(100) NOT NULL,
+  `depertment` varchar(60) NOT NULL,
+  `level` int(2) NOT NULL,
+  `tearm` int(2) NOT NULL,
+  `courseCode` varchar(40) NOT NULL,
+  `courseName` varchar(150) NOT NULL,
+  `courseCredit` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `depertments` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `universityName` varchar(100) NOT NULL,
+  `depertment` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+CREATE TABLE IF NOT EXISTS `universitys` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `universityName` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+);
