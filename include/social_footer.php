92beaefac90f7ccf5622993fe03530bb5a37ecd3
Mortuza Hossain <mortuzahossain1997@gmail.com>
2017-10-27 18:19:00 +0600

    Added social link

--- include/social_footer.php
+++ include/social_footer.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="text-center">
                 <a href="http://www.facebook.com/Mortuza99/" target="_blank"><i class="fb fa fa-facebook-square"></i></a>
-                <a href="#" target="_blank"><i class="yt fa fa-youtube-square"></i></a>
+                <a href="https://www.youtube.com/channel/UCX-myQM9f8FJsPaYm1xHgBQ" target="_blank"><i class="yt fa fa-youtube-square"></i></a>
                 <a href="tel:+8801719200957" target="_blank"><i class="ph fa fa-phone-square"></i></a>
             </div>
         </div>
